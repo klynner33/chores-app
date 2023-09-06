7d5279c569a831b543f91aaeeacad1cd32a6a1cb
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-09-05 22:48:52 -0500

    saving progress

---
+++ script.js
@@ -0,0 +1,27 @@
+class Day{
+morningChores = ["Get dressed", "Put away PJs", "Make bed", "Eat breakfast", "Get ready for the day", "Pack your water bottle**"];
+eveningChores = ["Pickup toy room/basement/yard", "Pack school snack**", "Check backpack for planner/folder/snack/shoes**", "Set out clothes for tomorrow", "Get ready for bed"];
+
+resetList
+
+
+}
+
+const monday = new Day();
+const tuesday = new Day();
+const wednesday = new Day();
+const thursday = new Day();
+const friday = new Day();
+const saturday = new Day();
+const sunday = new Day();
+
+class Child{
+    morningChores = ["Get dressed", "Put away PJs", "Make bed", "Eat breakfast", "Get ready for the day", "Pack your water bottle**"];
+    eveningChores = ["Pickup toy room/basement/yard", "Pack school snack**", "Check backpack for planner/folder/snack/shoes**", "Set out clothes for tomorrow", "Get ready for bed"];
+    
+    
+    }
+
+const Grace = new child();
+const Lettie = new child();
+const Miles = new child();
